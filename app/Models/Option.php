98a3bac2299ee9af910c101dfc538f49b6f951b0
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-10-18 13:20:24 -0300

    Cria controller,migrations e Models para as Pools(Enquetes) e Options(Opções)

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Options extends Model
+class Option extends Model
 {
     use HasFactory;
 
-    public function options(): BelongsTo
+    public function options() :BelongsTo
     {
         return $this->belongsTo(Pool::class);
     }
 }
+
+
--- app/Models/Pool.php
+++ app/Models/Pool.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pool extends Model
-{
+{   
     use HasFactory;
 
     public function options(): HasMany
     {
-        return $this->hasMany(Options::class);
+        return $this->hasMany(Option::class);
     }
 }

7982ca8afa9b64ad2342577cf233318c665d4abc
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-10-20 16:14:51 -0300

    melhorias de responsividade na página de votação

--- resources/views/components/display-time.blade.php
+++ resources/views/components/display-time.blade.php
@@ -1,6 +1,6 @@
 @props(['label'=>''])
 
-<div class="flex flex-col justify-between items-center space-y-2">
+<div class="flex flex-col justify-between items-center text-center space-y-1 md:space-y-2 w-min md:w-full">
     <p class="font-semibold text-md sm:text-lg">{{ $label }}</p>
     <div class="py-1 px-2 md:p-3 bg-surface-2 rounded-md border font-popins text-grape-400"><span>{{ $slot }}</span></div>
 </div>

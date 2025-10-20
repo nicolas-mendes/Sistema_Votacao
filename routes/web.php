9fd0a013b332474dc421ed47f7cfd2bb14029901
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-10-20 16:16:54 -0300

    modifica as rotas para Route::resource, gerando automaticamente todas as rotas básicas de um Controller

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,15 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/create',[PoolController::class,'create']);
-Route::post('/pools',[PoolController::class,'store']);
-Route::get('/show/{pool}', [PoolController::class, 'show'])->name('vote.show');
-Route::post('/show/{pool}', [PoolController::class, 'vote']);
+// GET           /pools                      index   pools.index
+// GET           /pools/create               create  pools.create
+// POST          /pools                      store   pools.store
+// GET           /pools/{pool}               show    pools.show
+// POST          /pools/{pool}               vote    pools.vote
+// GET           /pools/{pool}/edit          edit    pools.edit
+// PUT|PATCH     /pools/{pool}               update  pools.update
+// DELETE        /pools/{pool}               destroy pools.destroy
+Route::resource('pools', PoolController::class);
+
+
+Route::post('/pools/{pool}', [PoolController::class, 'vote'])->name('pools.vote');

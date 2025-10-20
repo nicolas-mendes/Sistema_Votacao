9c54b4be0127125dc96c7f6068fe918d61a914b3
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-10-20 16:22:26 -0300

    aplica uma lógica js de animação e funcionalidade do  menu mobile

--- resources/js/menu/mobileButton.js
+++ resources/js/menu/mobileButton.js
@@ -7,21 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     menuButton.addEventListener('click', () => {
       
-      // Alterna as classes de estado para a transição
-      mobileMenu.classList.toggle('opacity-0');       // De 0% para 100%
-      mobileMenu.classList.toggle('opacity-100');     // De 100% para 0%
-      mobileMenu.classList.toggle('-translate-y-4');  // De "para cima"
-      mobileMenu.classList.toggle('translate-y-0');   // Para "posição 0"
-      mobileMenu.classList.toggle('pointer-events-none'); // De "não clicável"
-      mobileMenu.classList.toggle('pointer-events-auto'); // Para "clicável"
+      mobileMenu.classList.toggle('opacity-0');   
+      mobileMenu.classList.toggle('opacity-100');     
+      mobileMenu.classList.toggle('-translate-y-4'); 
+      mobileMenu.classList.toggle('translate-y-0');
+      mobileMenu.classList.toggle('pointer-events-none');
+      mobileMenu.classList.toggle('pointer-events-auto'); 
 
-      // Alterna os ícones (hambúrguer e 'X')
       hamburgerIcon.classList.toggle('block');
       hamburgerIcon.classList.toggle('hidden');
       closeIcon.classList.toggle('block');
       closeIcon.classList.toggle('hidden');
 
-      // Atualiza o atributo ARIA para acessibilidade
       const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
       menuButton.setAttribute('aria-expanded', !isExpanded);
     });
